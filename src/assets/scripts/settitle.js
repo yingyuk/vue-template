72c14dbd55633ff1a7c78a7fb4d308d9167688bd
yingyuk <wuyingyucn@gmail.com>
2017-10-11 18:00:42 +0800

    设置标题

--- src/assets/scripts/settitle.js
+++ src/assets/scripts/settitle.js
@@ -2,18 +2,22 @@
  * 设置title，解决微信改不了title的bug
  *
  */
+const isServer = process.env.VUE_ENV === 'server';
 export default function setTitle(title) {
+  if (isServer) {
+    return;
+  }
   document.title = title;
   let userAgent = window.navigator.userAgent.toLowerCase();
   let isiOS = userAgent.indexOf('applewebkit') >= 0;
   let isWechat = userAgent.indexOf('micromessenger') >= 0;
   if (isiOS && isWechat) {
     let iframe = document.createElement('iframe');
-    iframe.src = 'https://www.baidu.com/favicon.ico';
+    iframe.src = '//www.baidu.com/favicon.ico';
     iframe.style.display = 'none';
     document.body.appendChild(iframe);
-    iframe.onload = function() {
-      setTimeout(function() {
+    iframe.onload = function () {
+      setTimeout(function () {
         iframe.remove();
       }, 0)
     }

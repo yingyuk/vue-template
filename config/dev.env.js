d464cfea0787624514aac7528df4087877bc8f1e
yingyuk <wuyingyucn@gmail.com>
2018-03-16 13:41:02 +0800

    fix: process.env

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,6 +1,6 @@
 const prodEnv = require('./prod.env');
 
-module.exports = Object.assign(prodEnv, {
+module.exports = Object.assign({}, prodEnv, {
   NODE_ENV: '"development"',
   VUE_ENV: '"development"',
 });
